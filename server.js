352fd82166b3e739b4241ac29759a2d7913b2f13
NicoDMTX <nicolas.demontoux@hotmail.com>
2021-04-14 17:40:58 +0200

    :art: adding the middleware to be able to check who is connected/ if someone is connected thanks to the token

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'})
 require('./config/db')
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 const app = express();
 
 
@@ -10,6 +12,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
+
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req,res) => {
+    res.status(200).send(res.locals.user._id)
+})
 
 // routes
 app.use('/api/user', userRoutes);
