8b884ce889b26f06ad08d7376b3403e63a8b0d11
NicoDMTX <nicolas.demontoux@hotmail.com>
2021-04-14 16:54:21 +0200

    :goal_net: error found, can connect with the route login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -28,11 +28,11 @@ module.exports.signIn = async (req, res) => {
         const user = await UserModel.login(email, password);
         const token = createToken(user._id)
         res.cookie('jwt', token, { httpOnly: true, maxAge})
-        res.status(200).json({ user: user_id})
+        res.status(200).json({ user: user._id})
     } catch(err){
-
+        res.status(200).json(err)
     }
-}
+};
 
 module.exports.logout = (req, res) => {
 
